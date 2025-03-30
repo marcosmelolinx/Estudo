4c229ae58d239cd03e2769512bf27563ddbbc564
MarcoMel0 <hshebehdje@gmail.com>
2025-03-30 02:21:29 -0300

    Inserido HandlerDepositar

--- Estudo/Program.cs
+++ Estudo/Program.cs
@@ -6,17 +6,19 @@ class Program
 {
     static void Main(string[] args)
     {
-        var dispatcher = new CommandDispatcher();
-        var conta = new ContaBancaria("Marcos", "Itau", 10);
+
+        var dispartcher = new CommandDispatcher();
+        var conta = new ContaBancaria("Marcos", "Itau",10);
 
         Console.WriteLine(conta);
 
         var sacarCommandHandler = new SacarCommandHandler();
-        dispatcher.RegisterHandler(sacarCommandHandler);
 
         var depositarCommand = new DepositarCommand(conta, 10);
-        dispatcher.Dispatch(depositarCommand);
+        dispartcher.Dispatch(depositarCommand);
 
         Console.ReadKey();
+
+
     }
 }
