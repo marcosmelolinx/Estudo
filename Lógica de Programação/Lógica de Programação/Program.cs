e68af8b45818f384e759faff5b1521b338b7e875
MarcosMel0 <marcos.melo@linx.com.br>
2025-04-14 20:56:58 -0300

    Inserido validação para validar se o número é misterioso(Não pode ser divisível por 5 e a soma de seus dígitos precisam ser par)

--- Lógica de Programação/Lógica de Programação/Program.cs
+++ Lógica de Programação/Lógica de Programação/Program.cs
@@ -3,12 +3,15 @@
 
 
 
+using Lógica_de_Programação;
+
 while (true){
     System.Console.WriteLine("\n Hello, World!");
 
     System.Console.WriteLine("O que deseja fazer?");
     System.Console.WriteLine("1 - Verificar se um número é perfeito");
     System.Console.WriteLine("2 - Verificar se um número é primo");
+    Console.WriteLine("3 - Verificar se um número é misterioso");
     System.Console.WriteLine("0 - Sair do programa");
 
     int opcao = int.Parse(Console.ReadLine()!);
@@ -22,6 +25,10 @@ while (true){
         case 2:
             VerificarPrimo verificarPrimo = new VerificarPrimo();
             verificarPrimo.VerificarEhPrimo();
+            break;
+        case 3:
+            VerificarNumeroMisterioso verificarNumeroMisterioso = new();
+            verificarNumeroMisterioso.ValidarNumeroMisterioso();
             break;
         case 0:
             System.Console.WriteLine("Você saiu do programa.");
---
+++ Lógica de Programação/Lógica de Programação/VerificarNumeroMisterioso.cs
@@ -0,0 +1,47 @@
+﻿namespace Lógica_de_Programação;
+
+public class VerificarNumeroMisterioso
+{
+    //A soma de todos os seus dígitos for um número par.
+    //E o número não for divisível por 5.
+    public void ValidarNumeroMisterioso()
+    {
+        Console.WriteLine("Digito o número inteiro: ");
+        int numero = int.Parse(Console.ReadLine());
+        if (NumeroDivisivel(numero) &&  EhSomaMistoriosa(numero))
+            Console.WriteLine("Número é misterioso");
+        else
+            Console.WriteLine("Número não é misterioso");
+            return;
+    }
+
+    public bool NumeroDivisivel(int numero)
+    {
+        if (numero % 5 == 0)
+        {
+            return false;
+        }
+        return true;
+
+    }
+
+    public bool EhSomaMistoriosa(int numero)
+    {
+        int Soma = 0;
+
+        while (numero > 0)
+        {
+            int digito = numero % 10;
+            Soma += digito;
+            numero = numero / 10;
+        }
+
+        if (Soma % 2 == 0)
+        {
+            return true;
+        }
+        else
+            return false;
+
+    }
+}
